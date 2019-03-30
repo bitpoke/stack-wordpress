3c862a09b09a3a0560643e1724cb62aa5b39d7d1
Calin Don <calin.don@gmail.com>
2019-03-30 20:59:07 +0200

    Fix dev wp-config.php symlink

--- web/index.php
+++ web/index.php
@@ -1,4 +1,10 @@
 <?php
+// This magic trick fixes our dev process. It's safe to create the `wordpress/wp-config.php` symlink since it's
+// gitignored.
+if (!file_exists(dirname(__DIR__) . '/wordpress/wp-config.php') && file_exists(__DIR__ . '/wp-config.php')) {
+    symlink(__DIR__ . '/wp-config.php', dirname(__DIR__) . '/wordpress/wp-config.php');
+}
+
 /** WordPress view bootstrapper */
 define('WP_USE_THEMES', true);
 require __DIR__ . '/wp/wp-blog-header.php';
