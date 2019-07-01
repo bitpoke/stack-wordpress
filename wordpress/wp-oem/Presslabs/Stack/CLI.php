f39c19d128d066e5368b171be647eb0a5f7451a2
Vlad Temian <vladtemian@gmail.com>
2019-07-01 15:26:30 +0300

    Use 5.2 as base docker image

--- wordpress/wp-oem/Presslabs/Stack/CLI.php
+++ wordpress/wp-oem/Presslabs/Stack/CLI.php
@@ -121,14 +121,14 @@ EOF;
     {
         $webroot = $webroot ?: $this->relativeWebroot();
         return <<<EOF
-FROM quay.io/presslabs/wordpress-runtime:5.1-latest as builder
+FROM quay.io/presslabs/wordpress-runtime:5.2-7.3.4-latest as builder
 RUN rm -rf /var/www/html
 COPY --chown=www-data:www-data . /var/www
 WORKDIR /var/www
 RUN composer install -n --no-ansi --no-dev --prefer-dist 
 RUN rm -rf .composer
 
-FROM quay.io/presslabs/wordpress-runtime:5.1-latest
+FROM quay.io/presslabs/wordpress-runtime:5.2-7.3.4-latest
 ENV DOCUMENT_ROOT=/var/www/$webroot
 RUN rm -rf /var/www/html
 COPY --from=builder /var/www /var/www
